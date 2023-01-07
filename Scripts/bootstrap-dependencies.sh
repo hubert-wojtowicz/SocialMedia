4ade95ca27a5334a778812a1c7d4e50fcea04b79
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2023-01-07 14:46:15 +0100

    Docs.

---
+++ Scripts/bootstrap-dependencies.sh
@@ -0,0 +1,12 @@
+sudo docker network create --attachable -d bridge smnetwork
+sudo docker network ls
+
+sudo docker run -it -d --name mongo-container -p 27017:27017 --network smnetwork --restart=always -v mongodb_data_container:/data/db mongo:latest
+
+# I was not able to pull image on 1/7/2023
+# https://learn.microsoft.com/en-us/sql/linux/quickstart-install-connect-docker?view=sql-server-ver16&pivots=cs1-bash
+docker run -d --name sql-container \
+--network smnetwork \
+--restart always \
+-e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=$tr0ngS@P@ssw0rd02' -e 'MSSQL_PID=Express' \
+-p 1433:1433 mcr.microsoft.com/mssql/server:2022-latest
