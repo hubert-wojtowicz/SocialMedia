a5ca49a65734274624c611ac8b6616d9004f3e34
Hubert Wójtowicz <hwojtowicz@eshopworld.com>
2023-01-08 19:08:13 +0100

    Implemented base of aggregate.

---
+++ src/CQRS-ES/CQRS.Core/Domain/AggregateRoot.cs
@@ -0,0 +1,48 @@
+﻿using CQRS.Core.Events;
+
+namespace CQRS.Core.Domain;
+
+public abstract class AggregateRoot
+{
+    protected Guid _id;
+    private readonly List<BaseEvent> _changes = new();
+
+    public Guid Id => _id;
+    public int Version { get; set; } = -1; // -1  means not set
+
+    public IEnumerable<BaseEvent> GetUncommittedChanges() => _changes;
+
+    public void MarkChangesAsCommitted()
+    {
+        _changes.Clear();
+    }
+
+    public void ReplyEvents(IEnumerable<BaseEvent> events)
+    {
+        foreach (var @event in events)
+        {
+            ApplyChange(@event, false);
+        }
+    }
+
+    protected void RaiseEvent(BaseEvent @event)
+    {
+        ApplyChange(@event, true);
+    }
+
+    private void ApplyChange(BaseEvent @event, bool isNew)
+    {
+        var method = this.GetType().GetMethod("Appply", new[] { @event.GetType() });
+        if (method == null)
+        {
+            throw new ArgumentNullException(nameof(method), $"Apply method was not found in aggregate for {@event.GetType().Name}.");
+        }
+
+        method.Invoke(this, new object[] { @event });
+
+        if (isNew)
+        {
+            _changes.Add(@event);
+        }
+    }
+}
