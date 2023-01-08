ab1b7e412af02009308e703d36d1617f4781eabb
Hubert Wójtowicz <hwojtowicz@eshopworld.com>
2023-01-08 18:01:43 +0100

    Add command and events.

---
+++ src/Post/Post.Cmd/Post.Cmd.Api/Commands/DeletePostCommand.cs
@@ -0,0 +1,9 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands;
+
+public class DeletePostCommand : BaseCommand
+{
+    public string Author { get; set; }
+    public string Message { get; set; }
+}
---
+++ src/Post/Post.Common/Events/CommentAddedEvent.cs
@@ -0,0 +1,15 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events;
+
+public class CommentAddedEvent : BaseEvent
+{
+    public CommentAddedEvent() : base(nameof(CommentAddedEvent))
+    {
+    }
+
+    public Guid CommentId { get; set; }
+    public string Author { get; set; }
+    public string Message { get; set; }
+    public DateTime CommentDate { get; set; }
+}
