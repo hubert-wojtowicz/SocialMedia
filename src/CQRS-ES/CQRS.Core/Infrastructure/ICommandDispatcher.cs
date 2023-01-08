de8126cb5714a8431f699be6f643ade5952ebe30
Hubert Wójtowicz <hwojtowicz@eshopworld.com>
2023-01-08 18:32:56 +0100

    Implement mediatior between commands and its dispatchers.

---
+++ src/CQRS-ES/CQRS.Core/Infrastructure/ICommandDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using CQRS.Core.Commands;
+
+namespace CQRS.Core.Infrastructure;
+
+/// <summary>
+/// Mediator interface
+/// </summary>
+public interface ICommandDispatcher
+{
+    void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand;
+    Task SendAsync(BaseCommand command);
+}
