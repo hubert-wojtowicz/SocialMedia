20545121f5d47ec25e60a4d53b06a2403a53147d
Hubert Wójtowicz <hwojtowicz@eshopworld.com>
2023-01-14 15:48:12 +0100

    Mongo repository has been created.

---
+++ src/Post/Post.Cmd/Post.Cmd.Infrastructure/Config/MongoDbConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Post.Cmd.Infrastructure.Config;
+
+public class MongoDbConfig
+{
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
+    public string CollectionName { get; set; }
+}
---
+++ src/Post/Post.Cmd/Post.Cmd.Infrastructure/Repositories/EventStoreRepository.cs
@@ -0,0 +1,32 @@
+﻿using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Post.Cmd.Infrastructure.Config;
+
+namespace Post.Cmd.Infrastructure.Repositories;
+
+public class EventStoreRepository : IEventStoreRepository
+{
+    private readonly IMongoCollection<EventModel> _eventsStoreCollection;
+
+    public EventStoreRepository(IOptions<MongoDbConfig> mongoDbOptions)
+    {
+        var config = mongoDbOptions.Value;
+        var mongoClient = new MongoClient(config.ConnectionString);
+        var mongoDatabase = mongoClient.GetDatabase(config.DatabaseName);
+        _eventsStoreCollection = mongoDatabase.GetCollection<EventModel>(config.CollectionName);
+    }
+
+    public async Task SaveAsync(EventModel @event)
+    {
+        await _eventsStoreCollection.InsertOneAsync(@event);
+    }
+
+    public async Task<List<EventModel>> GetByAggregateIdAsync(Guid aggregateId)
+    {
+        return await _eventsStoreCollection
+            .Find(x => x.AggregateIdentifier == aggregateId)
+            .ToListAsync();
+    }
+}
